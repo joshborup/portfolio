983aa203d30b828b06152aa3f78583ebece28800
joshborup <joshborup@gmail.com>
2018-06-01 00:06:52 +0000

    from server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
-const app = require('express')();
+const express = require('express');
+
 const bodyParser = require('body-parser');
 const contact = require('./controller/contact')
 require('dotenv').config();
+
+const app = express();
 app.use(bodyParser.json());
 
-
+app.use( express.static( `${__dirname}/../build` ) );
 app.post('/api/contact', contact.message)
+
+const path = require('path')
+ app.get('*', (req, res)=>{
+   res.sendFile(path.join(__dirname, '../build/index.html'));
+ })
+
+
 
 const port = 4000;
 app.listen(port, ()=> console.log(`listening on ${port}`))
