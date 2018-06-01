09c5553508b506d1450ef94063bab9f236262334
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-06-01 14:45:07 -0700

    updated index path in server file

--- server/index.js
+++ server/index.js
@@ -34,7 +34,9 @@ app.post('/portfolio_hook', (req, res) => {
 
 
 
-app.use( express.static( `${__dirname}/../build` ) );
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 
 
 const port = 4000;
