64a63d5f1e2e81647f5f69b0a6cd4f8b0597e96e
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-06-03 13:21:51 -0700

    updated server file

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.post('/portfolio_hook', (req, res) => {
 
 
 app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
+    res.sendFile(path.join(__dirname, '../build/'));
 })
 
 
