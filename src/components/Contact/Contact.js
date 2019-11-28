168e95d45cf3a3668dc6963778def1b6878c1ece
josh <joshborup@gmail.com>
2019-11-28 09:29:45 -0700

    adding form update to static html in public folder

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,12 +4,14 @@ const Contact = props => {
   return (
     <div className="contact-display">
       <h1>Contact Me</h1>
-      <form name="contact" method="POST" data-netlify="true">
+      <form name="contact" method="POST" netlify>
         <span>{props.state.result}</span>
+
         <input placeholder="NAME" name="name" />
         <input placeholder="E-MAIL" name="email" />
         <textarea placeholder="MESSAGE" name="message" />
         <button type="submit">SUBMIT!</button>
+        <input type="hidden" name="form-name" value="contact" />
       </form>
     </div>
   );
